d7322d1fe53ad0118d1bf8d6b9489855139662e7
AlejandroEchavarriaRIWI <aechavarriaj@gmail.com>
2024-11-06 06:35:38 -0500

    start

--- src/ui/atoms/Input.tsx
+++ src/ui/atoms/Input.tsx
@@ -19,7 +19,7 @@ export const Input = ({
         name={name}
         placeholder={placeholder}
         className={`px-4 py-2 rounded-lg text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
-          error ? "border-red-500" : "border-gray-300"
+          error ? "border-red-500" : "border-gray-300 focus:ring-red-500"
         }`}
         {...props}
       />
